a24a0b49304ee254f98dc486c0e986ac789275d8
KangIm <yunmo0501@naver.com>
2024-02-02 04:16:22 +0900

    RS485 commu func add

--- pi_proj_ym/pi_proj_ym/GPIO_control_node.py
+++ pi_proj_ym/pi_proj_ym/GPIO_control_node.py
@@ -1,20 +1,15 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from std_msgs.msg import Int32
+from std_msgs.msg import String
 import RPi.GPIO as GPIO
+import time
+import serial
+import threading
 
 led_pin = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin, GPIO.OUT)
-
-# PWM 설정
-pwm_frequency = 1000  # PWM 주파수 (Hz)
-pwm_duty_cycle = 0   # 초기 듀티 사이클 (0%로 시작)
-
-# PWM 객체 생성
-pwm_led = GPIO.PWM(led_pin, pwm_frequency)
-pwm_led.start(pwm_duty_cycle)
 
 class GPIO_control(Node):
 
@@ -22,31 +17,40 @@ class GPIO_control(Node):
         super().__init__('GPIO_control_node')
         qos_profile = QoSProfile(depth=10)
         self.GPIO_control_node = self.create_subscription(
-            Int32,
-            'GPIO_set',
+            String,
+            'Motor_control',
             self.subscribe_topic_message,
-            qos_profile)     
-        self.last_msg_data = 0
+            qos_profile)
+        self.last_msg_data = ""
         self.timer = self.create_timer(1.0, self.LED_control)
 
-    def subscribe_topic_message(self, msg):        
+        self.ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=3)
+
+    def subscribe_topic_message(self, msg):
         self.last_msg_data = msg.data
         print("Terminal Data Input: ", self.last_msg_data)
 
     def LED_control(self):
         input_data = self.last_msg_data
 
-        if input_data == 1:
+        if input_data == "Left":
             print("LED_control HIGH")
-            pwm_led.ChangeDutyCycle(100)  # 듀티 사이클을 100%로 설정하여 LED를 켬
-            
-        elif input_data == 0:
+            GPIO.output(led_pin, GPIO.HIGH)
+            self.send_serial_data("Left")  # RS485로 "Left" 데이터 송신
+
+        elif input_data == "Right":
             print("LED_control LOW")
-            pwm_led.ChangeDutyCycle(0)    # 듀티 사이클을 0%로 설정하여 LED를 끔
-            
+            GPIO.output(led_pin, GPIO.LOW)
+            self.send_serial_data("Right")  # RS485로 "Right" 데이터 송신
+
         else:
-            print("Data Error \n")
+            print("Data Empty \n")
             return
+
+    def send_serial_data(self, data):
+        str_cmd = '[' + data + ']'
+        print('send data=' + str_cmd)
+        self.ser.write(str_cmd.encode())
 
 def main(args=None):
     rclpy.init(args=args)
@@ -54,7 +58,6 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
-        pwm_led.stop()  # 프로그램 종료 시 PWM 정지
         GPIO.cleanup()
         node.get_logger().info('Keyboard Interrupt (SIGINT)')
     finally:
--- pi_proj_ym/pi_proj_ym/mode_select_node.py
+++ pi_proj_ym/pi_proj_ym/mode_select_node.py
@@ -1,15 +1,15 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, String
 
 class mode_select(Node):
 
     def __init__(self):
         super().__init__('mode_select_node')
         qos_profile = QoSProfile(depth=10)
-        self.GPIO_set_publisher = self.create_publisher(Int32, 'GPIO_set', qos_profile)
-        self.Arduino_set_publisher = self.create_publisher(Int32, 'Arduino_set', qos_profile)
+        self.motor_ctrl_publisher = self.create_publisher(String, 'Motor_control', qos_profile)
+        self.Arduino_set_publisher = self.create_publisher(String, 'Arduino_set', qos_profile)
         self.timer = self.create_timer(1, self.publish_msg)
         self.count = 0
 
@@ -17,22 +17,21 @@ class mode_select(Node):
         user_input = input("  1) GPIO 2) Arduino \n  모드를 선택하세요. : ")
         try:
             input_data = int(user_input)
-            rp_out = Int32()
-            ad_out = Int32()
+            rp_out = String()
+            ad_out = String()
             if input_data == 1:
-                rp_out.data = 1
-                ad_out.data = 0
-                self.GPIO_set_publisher.publish(rp_out)
+                rp_out.data = "Left"
+                ad_out.data = "Right"
+                self.motor_ctrl_publisher.publish(rp_out)
                 self.Arduino_set_publisher.publish(ad_out)
-                print("\n%d번 GPIO모드를 실행합니다.\n" %input_data)
+                print("\n%d번 GPIO모드를 실행합니다.\n" % input_data)
 
             elif input_data == 2:
-                rp_out.data = 0
-                ad_out.data = 1
-                self.GPIO_set_publisher.publish(rp_out)
+                rp_out.data = "Right"
+                ad_out.data = "Left"
+                self.motor_ctrl_publisher.publish(rp_out)
                 self.Arduino_set_publisher.publish(ad_out)
-                print("\n%d번 Arduino모드를 실행합니다.\n" %input_data)
-
+                print("\n%d번 Arduino모드를 실행합니다.\n" % input_data)
 
             else:
                 print("잘못된 입력입니다. 다시 입력해주세요. \n")
@@ -40,7 +39,6 @@ class mode_select(Node):
         except ValueError:
             print("잘못된 입력입니다. 다시 입력해주세요. \n")
             return
-
 
 def main(args=None):
     rclpy.init(args=args)
