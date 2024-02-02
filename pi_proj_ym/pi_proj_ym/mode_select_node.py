0471b067ed4ba0c04b727f92d9f379cfd0473b09
KangIm <yunmo0501@naver.com>
2024-02-03 05:22:24 +0900

    RS485 HEX send complete

--- pi_proj_ym/pi_proj_ym/mode_select_node.py
+++ pi_proj_ym/pi_proj_ym/mode_select_node.py
@@ -9,12 +9,12 @@ class mode_select(Node):
         super().__init__('mode_select_node')
         qos_profile = QoSProfile(depth=10)
         self.motor_ctrl_publisher = self.create_publisher(String, 'Motor_control', qos_profile)
-        self.Arduino_set_publisher = self.create_publisher(String, 'Arduino_set', qos_profile)
+        self.Arduino_set_publisher = self.create_publisher(String, 'spare_topic', qos_profile)
         self.timer = self.create_timer(1, self.publish_msg)
         self.count = 0
 
     def publish_msg(self):
-        user_input = input("  1) GPIO 2) Arduino \n  모드를 선택하세요. : ")
+        user_input = input("  1) Left 2) Right \n  모드를 선택하세요. : ")
         try:
             input_data = int(user_input)
             rp_out = String()
@@ -24,14 +24,14 @@ class mode_select(Node):
                 ad_out.data = "Right"
                 self.motor_ctrl_publisher.publish(rp_out)
                 self.Arduino_set_publisher.publish(ad_out)
-                print("\n%d번 GPIO모드를 실행합니다.\n" % input_data)
+                print("\n%d번 Left모드를 실행합니다.\n" % input_data)
 
             elif input_data == 2:
                 rp_out.data = "Right"
                 ad_out.data = "Left"
                 self.motor_ctrl_publisher.publish(rp_out)
                 self.Arduino_set_publisher.publish(ad_out)
-                print("\n%d번 Arduino모드를 실행합니다.\n" % input_data)
+                print("\n%d번 Right모드를 실행합니다.\n" % input_data)
 
             else:
                 print("잘못된 입력입니다. 다시 입력해주세요. \n")
