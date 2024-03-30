0abd291074c7bd9e6cfafc444c8151ae7c161a27
= <=>
2024-03-31 02:06:15 +0900

    communication load test code upload

--- image_processing/image_processing/dir_test/qos_test_cap.py
+++ image_processing/image_processing/dir_test/qos_test_cap.py
@@ -8,7 +8,6 @@ from rclpy.qos import QoSHistoryPolicy
 from rclpy.qos import QoSReliabilityPolicy
 from rclpy.qos import QoSDurabilityPolicy
 
-
 import cv2
 from cv_bridge import CvBridge
 
@@ -17,21 +16,59 @@ class TestCapture(Node):
 
     def __init__(self):
         super().__init__('communication_test_capture')
-        qos_profile = QoSProfile(depth=10)
-        self.image_publisher = self.create_publisher(Image, 'img_data', qos_profile)
-        self.timer = self.create_timer(0.1, self.image_capture)
+        qos_profile = QoSProfile(
+            # history=QoSHistoryPolicy.KEEP_LAST,
+            # reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            # durability=QoSDurabilityPolicy.VOLATILE,
+            depth=30
+        )
+        self.image_publisher0 = self.create_publisher(Image, 'img_data0', qos_profile)
+        self.image_publisher1 = self.create_publisher(Image, 'img_data1', qos_profile)
+        self.image_publisher2 = self.create_publisher(Image, 'img_data2', qos_profile)
+        self.image_publisher3 = self.create_publisher(Image, 'img_data3', qos_profile)
+        self.timer = self.create_timer(1/30, self.image_capture0)
+        self.timer = self.create_timer(1/30, self.image_capture1)
+        self.timer = self.create_timer(1/30, self.image_capture2)
+        self.timer = self.create_timer(1/30, self.image_capture3)
 
         self.cap = cv2.VideoCapture(0)
         self.cvbrid = CvBridge()
 
-    def image_capture(self):
+    def image_capture0(self):
         ret, img = self.cap.read()
 
         if not ret :
             self.get_logger().info('cannot detect camera')
         else :
-            img_resized = cv2.resize(img, dsize=(640,480), interpolation = cv2.INTER_AREA)
-            self.image_publisher.publish(self.cvbrid.cv2_to_imgmsg(img_resized))
+            # img_resized = cv2.resize(img, dsize=(640,480), interpolation = cv2.INTER_AREA)
+            self.image_publisher0.publish(self.cvbrid.cv2_to_imgmsg(img))
+            
+    def image_capture1(self):
+        ret, img = self.cap.read()
+
+        if not ret :
+            self.get_logger().info('cannot detect camera')
+        else :
+            # img_resized = cv2.resize(img, dsize=(640,480), interpolation = cv2.INTER_AREA)
+            self.image_publisher1.publish(self.cvbrid.cv2_to_imgmsg(img))
+            
+    def image_capture2(self):
+        ret, img = self.cap.read()
+
+        if not ret :
+            self.get_logger().info('cannot detect camera')
+        else :
+            # img_resized = cv2.resize(img, dsize=(640,480), interpolation = cv2.INTER_AREA)
+            self.image_publisher2.publish(self.cvbrid.cv2_to_imgmsg(img))
+            
+    def image_capture3(self):
+        ret, img = self.cap.read()
+
+        if not ret :
+            self.get_logger().info('cannot detect camera')
+        else :
+            # img_resized = cv2.resize(img, dsize=(640,480), interpolation = cv2.INTER_AREA)
+            self.image_publisher3.publish(self.cvbrid.cv2_to_imgmsg(img))
 
 
 
--- image_processing/image_processing/dir_test/qos_test_indi.py
+++ image_processing/image_processing/dir_test/qos_test_indi.py
@@ -16,21 +16,54 @@ class ImageIndicater(Node):
 
     def __init__(self):
         super().__init__('Image_indicater')
-        qos_profile = QoSProfile(depth=10)
-        self.image_subscribtion = self.create_subscription(
+        qos_profile = QoSProfile(
+            # history=QoSHistoryPolicy.KEEP_LAST,
+            # reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            # durability=QoSDurabilityPolicy.VOLATILE,
+            depth=30
+        )
+        self.image_subscribtion0 = self.create_subscription(
             Image,
-            'img_data',
-            self.sub_callback,
+            'img_data0',
+            self.sub_callback0,
+            qos_profile)
+        self.image_subscribtion1 = self.create_subscription(
+            Image,
+            'img_data1',
+            self.sub_callback1,
+            qos_profile)
+        self.image_subscribtion2 = self.create_subscription(
+            Image,
+            'img_data2',
+            self.sub_callback2,
+            qos_profile)
+        
+        self.image_subscribtion3 = self.create_subscription(
+            Image,
+            'img_data3',
+            self.sub_callback3,
             qos_profile)
         self.cvbrid = CvBridge()
 
-    def sub_callback(self, msg):
-
+    def sub_callback0(self, msg):
         current_img = self.cvbrid.imgmsg_to_cv2(msg)
-
-        cv2.imshow("title", current_img)
+        cv2.imshow("title0", current_img)
         cv2.waitKey(1)
 
+    def sub_callback1(self, msg):
+        current_img = self.cvbrid.imgmsg_to_cv2(msg)
+        cv2.imshow("title1", current_img)
+        cv2.waitKey(1)
+        
+    def sub_callback2(self, msg):
+        current_img = self.cvbrid.imgmsg_to_cv2(msg)
+        cv2.imshow("title2", current_img)
+        cv2.waitKey(1)
+        
+    def sub_callback3(self, msg):
+        current_img = self.cvbrid.imgmsg_to_cv2(msg)
+        cv2.imshow("title3", current_img)
+        cv2.waitKey(1)
 
 
 def main(args=None):
