ca285b169d35fe20cd6be820234040f8c3885852
= <=>
2024-07-03 23:40:43 +0900

    QoS profile upload

--- image_processing/image_processing/image_catcher.py
+++ image_processing/image_processing/image_catcher.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy, Duration
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
@@ -12,7 +12,13 @@ class ImageCatcher(Node):
 
     def __init__(self):
         super().__init__('Image_catcher')
-        qos_profile = QoSProfile(depth=10)
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            deadline=Duration(0.5),
+            )
         self.image_publisher = self.create_publisher(Image, 'img_data', qos_profile)
         self.timer = self.create_timer(1/24, self.image_capture)
 
--- image_processing/image_processing/image_indicater.py
+++ image_processing/image_processing/image_indicater.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy, Duration
 from std_msgs.msg import String
 
 from sensor_msgs.msg import Image
@@ -12,7 +12,13 @@ class ImageIndicater(Node):
 
     def __init__(self):
         super().__init__('Image_indicater')
-        qos_profile = QoSProfile(depth=10)
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            deadline=Duration(0.5),
+            )
         self.image_subscribtion = self.create_subscription(
             Image,
             'img_data',
