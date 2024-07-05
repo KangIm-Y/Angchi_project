325dc7ff130edad6db578bd3a16a1537da88a104
= <=>
2024-07-06 01:15:09 +0900

    setup

--- image_processing/image_processing/test_indicator.py
+++ image_processing/image_processing/test_indicator.py
@@ -23,8 +23,9 @@ class ImageIndicater(Node):
     def sub_callback(self, msg):
 
         current_img = self.cvbrid.imgmsg_to_cv2(msg)
+        resized = cv2.resize(current_img, (1280,720), interpolation=cv2.INTER_CUBIC)
 
-        cv2.imshow("title", current_img)
+        cv2.imshow("title", resized)
         cv2.waitKey(1)
 
 
