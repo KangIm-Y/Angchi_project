6771829dec9b6d44e57352555ef1b46c1b2a7d30
= <=>
2024-07-06 01:15:00 +0900

    resize size fix

--- image_processing/image_processing/img_faster/image_timer_pub.py
+++ image_processing/image_processing/img_faster/image_timer_pub.py
@@ -15,7 +15,7 @@ class ImageCatcher(Node):
         super().__init__('Image_catcher')
         qos_profile = QoSProfile(depth=10)
         self.image_publisher = self.create_publisher(Image, 'img_data', qos_profile)
-        self.timer = self.create_timer(1/30, self.image_capture)
+        self.timer = self.create_timer(1/24, self.image_capture)
         
         ### parameters ###
         
@@ -40,7 +40,7 @@ class ImageCatcher(Node):
             self.get_logger().info('cannot detect camera')
         else :
             start = time.time()
-            resized = cv2.resize(img, (320,180),interpolation=cv2.INTER_AREA)
+            resized = cv2.resize(img, (640,360),interpolation=cv2.INTER_AREA)
             self.image_publisher.publish(self.cvbrid.cv2_to_imgmsg(resized))
             end = time.time()
             self.get_logger().info(f'{(end- start)} resize and publish complete')
