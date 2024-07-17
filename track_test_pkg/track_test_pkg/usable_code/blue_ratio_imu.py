1f2153926ac4dcf5a3ae2c13cac80b9dfbfe5af1
skh <tls3162@naver.com>
2024-07-17 11:36:43 +0900

    multi threading imu code upload

---
+++ track_test_pkg/track_test_pkg/usable_code/blue_ratio_imu.py
@@ -0,0 +1,255 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Image
+
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+
+
+class BlueRatioCirculator(Node):
+
+    def __init__(self):
+        super().__init__('BlueRatio')
+        
+        self.video_group = MutuallyExclusiveCallbackGroup()
+        self.complement_group = MutuallyExclusiveCallbackGroup()
+        
+        qos_profile = QoSProfile(depth=10)
+        self.auto_control_publisher = self.create_publisher(
+            Float32MultiArray, 
+            'Odrive_control', 
+            qos_profile)
+        
+        self.img_publisher = self.create_publisher(
+            Image, 
+            'img_data', 
+            qos_profile)
+        
+        self.joy_subscriber = self.create_subscription(
+            Joy,
+            'joy',
+            self.joy_msg_sampling,
+            qos_profile,
+            callback_group=self.complement_group)
+        
+        self.imu_subscriber = self.create_subscription(
+            Float32MultiArray, 
+            'imu_data', 
+            self.imu_updater,
+            qos_profile,
+            callback_group=self.complement_group)
+        
+        ### parameters ###
+        cam_num = 4
+        self.U_detection_threshold = 140 ## 0~255
+        self.img_size_x = 1280
+        self.img_size_y = 720
+        self.ROI_ratio = 0.4
+        self.max_speed = 10
+        
+        self.odrive_mode = 1.
+        self.joy_status = False
+        self.joy_stick_data = [0, 0]
+        
+        self.angle_threshold = 25. #degree
+        ##################
+        
+        
+        ### declare parameters ###
+        self.before_R_joy = 0.
+        self.before_L_joy = 0.
+        self.L_joy = 0.
+        self.R_joy = 0.
+        
+        self.roll_angle = 0.
+        self.resized = np.zeros((self.img_size_y,self.img_size_x, 3), dtype = np.uint8)
+        
+        ##################
+        
+        
+        self.cap = cv2.VideoCapture(cam_num)
+        self.cvbrid = CvBridge()
+    
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.img_size_x)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.img_size_y)
+        
+        
+        self.image_timer = self.create_timer(1/24, self.image_capture, callback_group=self.video_group)
+        self.pub_timer = self.create_timer(1/24, self.publish_timer, callback_group=self.complement_group)
+        
+        self.get_logger().info('BlueRatioCirculator node has been initialized')
+        
+
+    def yuv_detection(self, img) :
+        gaussian = cv2.GaussianBlur(img, (3, 3), 1)
+        yuv_img = cv2.cvtColor(gaussian, cv2.COLOR_BGR2YUV)
+        Y_img, U_img, V_img = cv2.split(yuv_img)
+        
+        # rescale = np.clip(U_img - V_img, 0, 255).astype(np.uint8)
+        ret,U_img_treated = cv2.threshold(U_img, self.U_detection_threshold, 255, cv2.THRESH_BINARY)
+        if ret :
+            # filterd = cv2.bitwise_and(img, img, mask=U_img_treated)
+            # cv2.imshow("UUUU", filterd)
+            
+            contours, _ = cv2.findContours(U_img_treated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            max_area = 0
+            max_contour = None
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > max_area:
+                    max_area = area
+                    max_contour = contour
+
+            if max_contour is not None:
+                max_contour_mask = np.zeros_like(U_img_treated)
+                cv2.drawContours(max_contour_mask, [max_contour], -1, (255, 255, 255), thickness=cv2.FILLED)
+                
+            
+                filterd = cv2.bitwise_and(img, img, mask=max_contour_mask)
+                cv2.imshow("UUUU", filterd)
+                cv2.waitKey(1)
+        
+                histogram = np.sum(max_contour_mask, axis=0)
+                midpoint = int(self.img_size_x / 2)
+                L_histo = histogram[:midpoint]
+                R_histo = histogram[midpoint:]
+                # L_sum, R_sum = end_point_finder(max_contour_mask,histogram)
+                
+                L_sum = int(np.sum(L_histo) / 255)
+                R_sum = int(np.sum(R_histo) / 255)
+                
+                # print(f'{L_sum}   {R_sum}')
+                
+                return L_sum, midpoint, R_sum
+        return 1,1,1
+            
+            
+    def end_point_finder(self, binary_image,histogram) :
+        y,x = binary_image.shape
+        midpoint = int(x/2)
+        
+        L_histo = histogram[:midpoint]
+        R_histo = histogram[midpoint:]
+        
+        L_end = np.argmax(L_histo)
+        R_end = self.img_size_x - np.argmax(R_histo[::-1])
+        self.get_logger().info(f'L-diff {midpoint - L_end}   / R-diff {R_end - midpoint}')
+        return L_end, midpoint, R_end
+    
+    
+    ### timer 1/24
+    ### img main
+    def image_capture(self):
+        ret, img = self.cap.read()
+        
+        if not ret :
+            self.get_logger().info('cannot detect camera')
+        else :
+            
+            ROI = img[int(self.img_size_y * (1-self.ROI_ratio)):,:].copy()
+            cv2.rectangle(img,(0,int(self.img_size_y * (1-self.ROI_ratio))),(self.img_size_x, self.img_size_y),(255,0,0),2)
+            cv2.line(img,(int(self.img_size_x / 2 ),int(self.img_size_y * (1-self.ROI_ratio))),(int(self.img_size_x / 2 ), self.img_size_y),(255,0,0),2)
+            cv2.imshow("origin", img)
+            cv2.waitKey(1)
+            L_sum, midpoint, R_sum = self.yuv_detection(ROI)
+                
+            
+            if self.joy_status == True :
+                self.L_joy = (self.joy_stick_data[0] * 10)
+                self.R_joy = (self.joy_stick_data[1] * 10)
+                    
+            elif (self.roll_angle <= -self.angle_threshold) : ## minus is left
+                if (L_sum <= R_sum*1.2) :
+                    self.L_joy = (self.max_speed * (R_sum*1.2/(R_sum*1.2+L_sum)))
+                    self.R_joy = (self.max_speed * (L_sum/(R_sum*1.2+L_sum)))
+                elif (L_sum >= R_sum) :
+                    self.L_joy = (self.max_speed * (R_sum/(R_sum+L_sum*1.2)))
+                    self.R_joy = (self.max_speed * (L_sum*1.2/(R_sum+L_sum*1.2)))
+                    
+            elif (self.roll_angle >= self.angle_threshold)  : ## plus is right
+                if (R_sum <= L_sum*1.2) :
+                    self.L_joy = (self.max_speed * (R_sum/(R_sum+L_sum*1.2)))
+                    self.R_joy = (self.max_speed * (L_sum*1.2/(R_sum+L_sum*1.2)))
+                elif (R_sum >= L_sum) :
+                    self.L_joy = (self.max_speed * (R_sum*1.2/(R_sum*1.2+L_sum)))
+                    self.R_joy = (self.max_speed * (L_sum/(R_sum*1.2+L_sum)))
+                    
+            elif(((L_sum < R_sum*1.1) & (L_sum > R_sum*0.9)) | ((R_sum < L_sum*1.1) & (R_sum > L_sum*0.9))) :
+                self.L_joy = (self.max_speed / 2)
+                self.R_joy = (self.max_speed / 2)
+            elif ((L_sum < R_sum*0.25) | (R_sum < L_sum*0.25)) :
+                self.L_joy = (self.max_speed / 1.25 ) * (0.25 if L_sum > R_sum else 1.)
+                self.R_joy = (self.max_speed / 1.25 ) * (0.25 if L_sum < R_sum else 1.)
+            elif ((L_sum > R_sum) | (R_sum > L_sum)) :
+                self.L_joy = (self.max_speed * (R_sum/(R_sum+L_sum)))
+                self.R_joy = (self.max_speed * (L_sum/(R_sum+L_sum)))
+            else :
+                self.L_joy = self.before_L_joy
+                self.R_joy = self.before_R_joy
+            
+            self.get_logger().info(f'{self.L_joy}   {self.R_joy}')
+            
+            
+            resized = cv2.resize(img, (320,180),interpolation=cv2.INTER_AREA)
+            self.img_publisher.publish(self.cvbrid.cv2_to_imgmsg(resized))
+            # gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+            
+            
+            # msg.data = [L_sum, midpoint, R_sum]
+            # self.image_publisher.publish(msg)
+
+    
+    def joy_msg_sampling(self, msg):
+        axes = msg.axes
+        # btn = msg.buttons
+
+        if axes[2] != -1 :
+            self.joy_status = False
+        else :
+            self.joy_status = True
+            self.joy_stick_data = [axes[1], axes[4]]
+            
+            
+    def publish_timer(self) :
+        msg = Float32MultiArray()
+        msg.data = [self.odrive_mode, self.L_joy, self.R_joy]
+        self.auto_control_publisher.publish(msg)
+        
+        
+        self.before_R_joy = self.R_joy
+        self.before_L_joy = self.L_joy
+        
+            
+    def imu_updater(self, msg) :
+        imu_data = msg.data
+        self.roll_angle = imu_data[0]
+            
+            
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = BlueRatioCirculator()
+    executor = MultiThreadedExecutor(num_threads=4)
+    executor.add_node(node)
+    
+    try:
+        # Spin the executor
+        executor.spin()
+    finally:
+        # Shutdown the executor
+        executor.shutdown()
+        node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
