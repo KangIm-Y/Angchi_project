b98bbd07565cba4bcc846b801ce1b2f6e3d158e4
pas123068 <pas123068@gmail.com>
2024-07-21 09:26:42 +0900

    mani urdf package

--- rs485_mani/launch/pub_joint.launch.py
+++ rs485_mani/launch/pub_joint.launch.py
@@ -6,5 +6,6 @@ def generate_launch_description():
         Node(
             package='rs485_mani', # 실행할 ROS2 프로그램의 코드가 포함된 패키지의 이름
             executable='pub_joint', # 실행하려는 Python 실행 파일의 이름
+            emulate_tty=True,
             output='screen'), # 프로그램의 출력을 출력할 채널
     ])
