a9bee2c64cac28250988be4663c2e79fa70e80e3
pas123068 <pas123068@gmail.com>
2024-07-27 02:47:26 +0900

    manipulator coordinate control complete + dynamixel pub

--- call_srv/call_srv/caller.py
+++ call_srv/call_srv/caller.py
@@ -1,5 +1,6 @@
 # import the PositionService module from custom_interfaces package
 from custom_interfaces.srv import PositionService
+from std_srvs.srv import SetBool
 # import the ROS2 Python client libraries
 import rclpy
 from rclpy.node import Node
@@ -16,13 +17,18 @@ class ClientAsync(Node):
         # create the Service Client object
         # defines the name and type of the Service Server you will work with.
         self.client = self.create_client(PositionService, 'pos_srv')
+        self.client1 = self.create_client(SetBool, 'ActiveGripper')
         # checks once per second if a Service matching the type and name of the client is available.
         while not self.client.wait_for_service(timeout_sec=1.0):
             # if it is not available, a message is displayed
-            self.get_logger().info('service not available, waiting again...')
+            self.get_logger().info('Moveit service not available, waiting again...')
+        while not self.client1.wait_for_service(timeout_sec=1.0):
+            # if it is not available, a message is displayed
+            self.get_logger().info('Gripper service not available, waiting again...')
         
         # create an Empty request
         self.req = PositionService.Request()
+        self.grip = SetBool.Request()
         
 
     def send_request(self):
@@ -37,8 +43,15 @@ class ClientAsync(Node):
         self.future = self.client.call_async(self.req)
         # to print in the console
 
+    def active_gripper(self, grip):
+        # send the request
+        self.grip.data = grip
+        # uses sys.argv to access command line input arguments for the request.
+        self.future1 = self.client1.call_async(self.grip)
+        # to print in the console
 
 def main(args=None):
+    moved = False
     # initialize the ROS communication
     rclpy.init(args=args)
     # declare the node constructor
@@ -49,13 +62,16 @@ def main(args=None):
     while rclpy.ok():
         # pause the program execution, waits for a request to kill the node (ctrl+c)
         rclpy.spin_once(client)
-        if client.future.done():
+        if client.future.done() and moved == False:
             try:
                 # checks the future for a response from the Service
                 # while the system is running. 
                 # if the Service has sent a response, the result will be written
                 # to a log message.
                 response = client.future.result()
+                if response == True:
+                    moved = True
+                    client.active_gripper(True)
             except Exception as e:
                 # Display the message on the console
                 client.get_logger().info(
@@ -64,7 +80,25 @@ def main(args=None):
                 # Display the message on the console
                 client.get_logger().info(
                     'Response state %r' % (response.success,))
-            break
+        if client.future1.done() and moved == True:
+            try:
+                # checks the future for a response from the Service
+                # while the system is running. 
+                # if the Service has sent a response, the result will be written
+                # to a log message.
+                response1 = client.future1.result()
+                if response1 == True:
+                    moved = False
+            except Exception as e:
+                # Display the message on the console
+                client.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                # Display the message on the console
+                client.get_logger().info(
+                    'Response state %r' % (response1.success,))
+                break
+            
 
     client.destroy_node()
     # shutdown the ROS communication
