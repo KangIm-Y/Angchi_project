b42ff2d9229c7cabcf3ede4777ee6732921bd313
lattepanda <tls3162@naver.com>
2024-08-28 05:27:51 +0900

    commit

--- track_test_pkg/track_test_pkg/testing/col_checker.py
+++ track_test_pkg/track_test_pkg/testing/col_checker.py
@@ -142,7 +142,7 @@ class BlueRatioCirculator(Node):
         ret,U_img_treated = cv2.threshold(U_img, self.U_detection_threshold, 255, cv2.THRESH_BINARY)
         
         # resized = cv2.resize(U_img_treated, (424,240),interpolation=cv2.INTER_AREA)
-        self.img_publisher.publish(self.cvbrid.cv2_to_imgmsg(U_img_treated))
+        # self.img_publisher.publish(self.cvbrid.cv2_to_imgmsg(U_img_treated))
         if ret :
             # filterd = cv2.bitwise_and(img, img, mask=U_img_treated)
             # cv2.imshow("UUUU", filterd)
@@ -175,6 +175,7 @@ class BlueRatioCirculator(Node):
                 R_sum = int(np.sum(R_histo) / 255) - y
                 
                 # print(f'{L_sum}   {R_sum}')
+                self.img_publisher.publish(self.cvbrid.cv2_to_imgmsg(filterd))
                 
                 return L_sum, midpoint, R_sum
         return 1,1,1
