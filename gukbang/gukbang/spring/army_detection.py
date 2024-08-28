fcd35859bb0803afe749c50542217f274665f808
lattepanda <tls3162@naver.com>
2024-08-29 03:37:52 +0900

    army detection code fix

--- gukbang/gukbang/spring/army_detection.py
+++ gukbang/gukbang/spring/army_detection.py
@@ -11,16 +11,18 @@ class ArmyDetectionNode(Node):
     def __init__(self):
         super().__init__('army_detection_node')
         self.bridge = CvBridge()
-        # self.model = YOLO('/home/skh/robot_ws/src/gukbang/gukbang/common/army.pt')  # 모델 파일 위치 .pt
-        self.model = YOLO('/home/lattepanda/robot_ws/src/gukbang/gukbang/common/army.pt')  # 모델 파일 위치 .pt
+        # self.model = YOLO('/home/skh/robot_ws/src/gukbang/gukbang/common/army.pt') # main laptop
+        self.model = YOLO('/home/lattepanda/robot_ws/src/gukbang/gukbang/common/army.pt')  #lattepanda
         # /home/lattepanda/robot_ws/src/gukbang/gukbang/common
         
         qos_profile = QoSProfile(depth=10)
         self.publisher = self.create_publisher(Image,'side_camera',qos_profile)
         
         ### parameter setting ###
-        self.img_size_x = 648
-        self.img_size_y = 480
+        self.img_size_x = 1280
+        self.img_size_y = 720
+
+        self.frame_rate = 15
         
         
         
@@ -35,8 +37,10 @@ class ArmyDetectionNode(Node):
         self.cap0.set(cv2.CAP_PROP_FRAME_HEIGHT, self.img_size_y)
         self.cap1.set(cv2.CAP_PROP_FRAME_WIDTH, self.img_size_x)
         self.cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.img_size_y)
+        self.cap0.set(cv2.CAP_PROP_FPS, self.frame_rate)
+        self.cap1.set(cv2.CAP_PROP_FPS, self.frame_rate)
         
-        self.timer_finder = self.create_timer(1/24, self.image_callback)
+        self.timer_finder = self.create_timer(1/self.frame_rate, self.image_callback)
 
     def image_callback(self):
         
@@ -49,7 +53,7 @@ class ArmyDetectionNode(Node):
             if not ret1 :
                 print(f'cam1 is cannot connetion')
         else :
-            frame = np.hstack((img0, img1))
+            frame = np.vstack((img0, img1))
             
             result = self.model.predict(frame, conf = 0.4, verbose=False)
             
@@ -61,14 +65,15 @@ class ArmyDetectionNode(Node):
                     color = [255,255,255]
                     if label == 0 :
                         color =[0,255,0]
-                        cv2.putText(frame, f'Korea ARMY  {(confidence*100):.2f}%', (object_xyxy[0], object_xyxy[1] - 20),cv2.FONT_ITALIC, 1, (255, 255, 255), 2)
+                        cv2.putText(frame, f'Korea ARMY  {(confidence*100):.2f}%', (object_xyxy[0], object_xyxy[1] - 20),cv2.FONT_ITALIC, 1, (0, 255, 0), 2)
                     else :
                         color = [0,0,255]
-                        cv2.putText(frame, f'ENEMY  {(confidence*100):.2f}%', (object_xyxy[0], object_xyxy[1] - 20), cv2.FONT_ITALIC, 1, (255, 255, 255), 2)
+                        cv2.putText(frame, f'ENEMY  {(confidence*100):.2f}%', (object_xyxy[0], object_xyxy[1] - 20), cv2.FONT_ITALIC, 1, (0, 0, 255), 2)
                     cv2.rectangle(frame, (object_xyxy[0], object_xyxy[1]), (object_xyxy[2], object_xyxy[3]), color, 2)
             
             
-            
+            resized = cv2.resize(frame, (int(self.img_size_x/2),int(self.img_size_y)),interpolation=cv2.INTER_AREA)
+            self.publisher.publish(self.cvbrid.cv2_to_imgmsg(resized))
             cv2.imshow("Object Detection1", frame)
             cv2.waitKey(1)  # Adjust the waitKey value for the desired frame display time
 
