0f4081b2de7b2c7223f618f1847ea03ef71a224c
skh <tls3162@naver.com>
2024-08-29 08:21:08 +0900

    aruco complete

--- gukbang/gukbang/autumn/aruco_detection.py
+++ gukbang/gukbang/autumn/aruco_detection.py
@@ -4,8 +4,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
-from ultralytics import YOLO
-from rclpy.qos import QoSProfile
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
 import cv2.aruco as aruco
 
@@ -14,19 +13,32 @@ class ArucoDetection(Node):
         super().__init__('aruco_detection')
         self.bridge = CvBridge()
         
-        qos_profile = QoSProfile(depth=10)
+        qos_profile = QoSProfile(
+        reliability=ReliabilityPolicy.BEST_EFFORT,
+        history=HistoryPolicy.KEEP_LAST,
+        depth=10  
+        )
+        
         self.publisher = self.create_publisher(Image,'side_camera',qos_profile)
         
-        ### parameter setting ###
-        self.img_size_x = 1280
-        self.img_size_y = 720
         
+        
+        ### parameter setting ###
+        self.img_size_x = 640
+        self.img_size_y = 480
+        self.frame_rate = 10
+        
+        ########################
+        
+        ### params ###
+        self.threshold = 120
+        ##############
         
         ### aruco setting ###
         
         self.aruco_dict = aruco.Dictionary_create(9, 5)
         self.aruco_dict.bytesList = np.empty(shape=(9, 4, 4), dtype=np.uint8)
-        # Define markers
+        
         self.markers = [
             [[1,0,0,0,1],[1,0,0,1,0],[1,1,1,0,0],[1,0,0,1,0],[1,0,0,0,1]],  # K
             [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]],  # O
@@ -48,16 +60,18 @@ class ArucoDetection(Node):
         #########################
         
         
-        self.cap0 = cv2.VideoCapture(0)  #cam0
-        self.cap1 = cv2.VideoCapture(2)  #cam1
+        self.cap0 = cv2.VideoCapture('/dev/cam0')  #cam0
+        self.cap1 = cv2.VideoCapture('/dev/cam1')  #cam1
         self.cvbrid = CvBridge()
         
         self.cap0.set(cv2.CAP_PROP_FRAME_WIDTH, self.img_size_x)
         self.cap0.set(cv2.CAP_PROP_FRAME_HEIGHT, self.img_size_y)
         self.cap1.set(cv2.CAP_PROP_FRAME_WIDTH, self.img_size_x)
         self.cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.img_size_y)
+        self.cap0.set(cv2.CAP_PROP_FPS, self.frame_rate)
+        self.cap1.set(cv2.CAP_PROP_FPS, self.frame_rate)
         
-        self.timer_finder = self.create_timer(1/24, self.image_callback)
+        self.timer_finder = self.create_timer(1/self.frame_rate, self.image_callback)
 
     def image_callback(self):
         
@@ -71,14 +85,16 @@ class ArucoDetection(Node):
                 print(f'cam1 is cannot connetion')
         else :
             gray0 = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
-            # gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+            gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
             
-            bw0 = cv2.threshold(gray0, 100, 255, cv2.THRESH_BINARY)[1]
-            # bw1 = cv2.threshold(gray1, 128, 255, cv2.THRESH_BINARY)[1]
+            bw0 = cv2.threshold(gray0, self.threshold, 255, cv2.THRESH_BINARY)[1]
+            bw1 = cv2.threshold(gray1, self.threshold, 255, cv2.THRESH_BINARY)[1]
             
             corners0, ids0, points0 = aruco.detectMarkers(bw0, self.aruco_dict, parameters=self.aruco_param)
+            corners1, ids1, points1 = aruco.detectMarkers(bw1, self.aruco_dict, parameters=self.aruco_param)
             
             img0 = aruco.drawDetectedMarkers(img0, corners0)
+            img1 = aruco.drawDetectedMarkers(img1, corners0)
             
                 
             # Draw custom text
@@ -87,15 +103,30 @@ class ArucoDetection(Node):
                     corner = corners0[i][0]
                     top_left = (int(corner[0][0]), int(corner[0][1]))
                     bottom_right = (int(corner[2][0]), int(corner[2][1]))
-                    # center = (int((top_left[0] + bottom_right[0]) / 2), int((top_left[1] + bottom_right[1]) / 2))
                     
                     marker_id = ids0[i][0]
                     if marker_id < len(self.marker_chars):
-                        cv2.putText(img0, self.marker_chars[marker_id], top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                        cv2.putText(img0, self.marker_chars[marker_id], top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                        
+                        
+            if ids1 is not None:
+                for i in range(len(ids1)):
+                    corner = corners1[i][0]
+                    top_left = (int(corner[0][0]), int(corner[0][1]))
+                    bottom_right = (int(corner[2][0]), int(corner[2][1]))
+                    
+                    marker_id = ids1[i][0]
+                    if marker_id < len(self.marker_chars):
+                        cv2.putText(img1, self.marker_chars[marker_id], top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                        
+            
+            frame = np.vstack((img0, img1))
+            
+            resized = cv2.resize(frame, (int(self.img_size_x/2),int(self.img_size_y)),interpolation=cv2.INTER_AREA)
+            self.publisher.publish(self.cvbrid.cv2_to_imgmsg(resized))
         
-        # Display the resulting frame
-        cv2.imshow('bw', bw0)
-        cv2.imshow('frame', img0)
+        
+        cv2.imshow('frame', frame)
         cv2.waitKey(1)
 
 def main(args=None):
