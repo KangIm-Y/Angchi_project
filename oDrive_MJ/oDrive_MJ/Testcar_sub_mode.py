e70c5705e2dfef219100db813e22c7f7d4fb6938
lattepanda <tls3162@naver.com>
2024-09-01 15:01:40 +0900

    upload

--- oDrive_MJ/oDrive_MJ/Testcar_sub_mode.py
+++ oDrive_MJ/oDrive_MJ/Testcar_sub_mode.py
@@ -53,8 +53,8 @@ class Testcar_sub(Node):
             ########################## Ramped Velocity Control #############################
             msg.data[1] = -msg.data[1]
             
-            self.my_drive.axis0.controller.input_vel = msg.data[1]
-            self.my_drive.axis1.controller.input_vel = msg.data[2]
+            self.my_drive.axis0.controller.input_vel = - msg.data[2]
+            self.my_drive.axis1.controller.input_vel = - msg.data[1]
             self.get_logger().info('Velocity control set: axis0 = {}, axis1 = {}'.format(msg.data[1], msg.data[2]))
             self.get_logger().info('Moving...by Ramped Velocity')
             
