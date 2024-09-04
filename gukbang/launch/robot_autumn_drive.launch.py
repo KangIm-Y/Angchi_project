a22281b8dec34c5643d36e265b19979ad1033f3e
skh <tls3162@naver.com>
2024-09-04 18:32:47 +0900

    upsh

--- gukbang/launch/robot_autumn_drive.launch.py
+++ gukbang/launch/robot_autumn_drive.launch.py
@@ -4,6 +4,11 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='gukbang',  # 첫 번째 노드의 패키지 이름
+            executable='aruco_detection',  # 첫 번째 노드의 실행 파일 이름
+            output='screen'  # 첫 번째 노드의 출력 설정
+        ),
+        Node(
             package='gukbang',  
             executable='joy_drive',  
             output='screen'  
@@ -11,11 +16,6 @@ def generate_launch_description():
         Node(
             package='gukbang',  
             executable='odrive',  
-            output='screen'  
-        ),
-        Node(
-            package='gukbang',  
-            executable='joy_drive_sub_cam',  
             output='screen'  
         ),
     ])
